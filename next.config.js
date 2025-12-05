c4b57f09dd0cd11e72df730a107ed7f82585b345
Rango <wsx4588588@gmail.com>
2025-12-05 15:45:20 +0800

    feat: add worker

--- next.config.js
+++ next.config.js
@@ -8,9 +8,18 @@ const nextConfig = {
         port: '3001',
         pathname: '/uploads/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pub-5bf2f52797de4587a60587bc9dbfb63f.r2.dev',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'canlog-backend.onrender.com',
+        pathname: '/**',
+      },
     ],
   },
 };
 
 module.exports = nextConfig;
-
